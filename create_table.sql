39a99e9edaca79cda1499411752721e99e3cb474
AbdulDunggio <dunggioalil53@gmail.com>
2025-01-22 19:48:48 +0700

    Create create_table.sql
    make table as a locate cv data

---
+++ create_table.sql
@@ -0,0 +1,25 @@
+SHOW databases;
+
+USE my_portfolio
+
+SHOW tables;
+
+CREATE TABLE car_info(
+   id INT,
+   name VARCHAR(255),
+   price int,
+   Brand VARCHAR(255),
+   Model VARCHAR(255),
+   Variant VARCHAR(255),
+   Series VARCHAR(255),
+   Year int,
+   Kilometer int,
+   Type VARCHAR(255),
+   Gearbox VARCHAR(255),
+   Fuel VARCHAR(255),
+   Status Varchar(255),
+   CC int,
+   color VARCHAR(255),
+   Seating_Capacity INT  
+);
+
